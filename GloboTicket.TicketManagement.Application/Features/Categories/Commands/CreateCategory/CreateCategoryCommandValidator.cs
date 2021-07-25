9d4f9d417e0c64862ea6e6baa7252612087f186f
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2021-07-25 19:44:07 -0300

    Custom Response Catogory

---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory
+{
+    internal class CreateCategoryCommandValidator
+    {
+        public CreateCategoryCommandValidator()
+        {
+        }
+
+        internal Task ValidateAsync(CreateCategoryCommand request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryDto.cs
@@ -0,0 +1,6 @@
+﻿namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory
+{
+    internal class CreateCategoryDto
+    {
+    }
+}
